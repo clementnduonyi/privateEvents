70b9da5f5a905cf90625c2f5147897f8f19e3e95
Cendevs <cndigitech@gmail.com>
2021-07-15 00:18:25 +0100

    refactored and uses enum status.

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,18 +1,44 @@
 class AttendancesController < ApplicationController
+    before_action :authenticate_user!, only: [:show]
 
-    def attendance
-        event = Event.find(params[:event_id])
-        attendances = Attendance.new(event_id: event.id, attendee_id: attendee(params[:attendee_id]))
+  def create
+      event = Event.find(params[:event_id])
+      attendance = Attendance.new(event_id: event.id, attendee_id: params[:attendee_id])
 
-        if attendances.save
-            attendances.invite_sent!
-            flash[:notice] = "Invited successfully"
-            redirect_to users_path(event_id: event.id)
-        else
-            flash[:alert] = "Something went wrong! contact event owner"
-            redirect_to event_path(event)
-        end
+      if attendance.save
+          attendance.invite_sent!
+          flash[:notice] = "Invited successfully"
+          redirect_to users_path(event_id: event.id)
+      else
+          flash[:alert] = "Something went wrong! contact event owner"
+          redirect_to event_path(event)
+      end
+  end
+
+  def destroy
+    event = Event.find(params[:event_id])
+    attendance = Attendance.find(params[:id])
+    if current_user == event.creator
+        attendance.destroy
+        flash[:notice] = "The invitation is cancelled!"
+    else
+        attendance.invite_sent!
+        flash[:notice] = "You have indicated to attend #{@event.theme} event!"
     end
 
+    redirect_to users_path(event_id: event.id, id: attendance.id)
+  end
 
+  def update
+    @event = Event.find(params[:event_id])
+    @attendance = Attendance.find_by(event_id: params[:event_id], attendee: current_user.id)
+    if @attendance && @attendance.invite_sent?
+      @attendance.attending!
+      flash[:notice] = "Thank you for signing up for the '#{@event.theme}'!"
+    else
+      flash[:alert] = 'Your name is not on the invitation list'
+    end
+
+    redirect_to event_path(@event)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: "registrations" }
   resources :events
   resources :users
+  resources :attendances, only: [:create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "events#index"
   authenticated :user do
