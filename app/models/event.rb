1372a5cfdbf61bd0ea679202f7cc3ecfea7b16c0
Cendevs <cndigitech@gmail.com>
2021-07-17 02:24:43 +0100

    add styling and authentication

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
     has_many :attendances
     has_many :attendees, through: :attendances, source: :user
+
+    scope :upcoming, -> { where('event_end_date >= ?', Date.today) }
+    scope :past, -> { where('event_end_date < ?', Date.today) }
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module PrivateEvents
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.assets.paths << Rails.root.join("app", "assets", "images")
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
