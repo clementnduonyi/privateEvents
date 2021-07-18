6b1a49b501ae5cb5198ed84cc04dd53b6da55caf
Cendevs <cndigitech@gmail.com>
2021-07-18 01:10:11 +0100

    add validations and more styles

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,8 @@ class Event < ApplicationRecord
 
     scope :upcoming, -> { where('event_end_date >= ?', Date.today) }
     scope :past, -> { where('event_end_date < ?', Date.today) }
+
+    validates :theme, :venue, :details, presence: true
+    validates :event_start_date, :event_end_date, presence: true
+    validates :event_start_time, :event_end_time, presence: true
 end
